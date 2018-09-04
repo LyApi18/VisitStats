1e3328688290ea4d2b9a5995f87f8f708ff73dc5
xiaozhuox <mrxzx@wwsg18.top>
2018-09-04 17:58:05 +0800

    Commit Code

---
+++ src/Extend.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace LyApi\extend\VisitStats;
+
+use LyApi\cache\FileCache;
+
+class Extend{
+    private $FileCache;
+
+    public function __construct(){
+        $this->FileCache = new FileCache("VisitStats");
+    }
+
+    //获取统计数量
+    public function GetStats($key = 'all'){
+        $this->FileCache->get($key);
+    }
+
+    //增加统计数量
+    public function AddStats($key = 'all'){
+        $num = 1;
+        if($this->FileCache->has($key)){
+            $num = $this->FileCache->get($key);
+            $num = (int)$num;
+            $num++;
+        }
+        $this->FileCache->set($key,$num);
+    }
+}
